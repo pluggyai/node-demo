ebfaaffcdf34a85ffc071197faf958a03f031a02
Federico Miras <miras.federico@gmail.com>
2021-05-23 01:08:35 -0300

    fix: make more accurate the authentication via security token

--- api/items.ts
+++ api/items.ts
@@ -4,10 +4,10 @@ import { getItems } from '../lib/items'
 export default async (req: VercelRequest, res: VercelResponse) => {
   const { from, skip, size } = req.query
   const { authorization } = req.headers
-  const { SERVER_AUTH } = process.env
+  const { SERVER_AUTH_TOKEN } = process.env
 
   // If Authorization is required validate that the token is sent in the header
-  if (SERVER_AUTH && (!authorization || !authorization.includes(SERVER_AUTH))) {
+  if (SERVER_AUTH_TOKEN && (!authorization || authorization !== `Bearer ${SERVER_AUTH_TOKEN}`)) {
     res.status(401).json({ message: 'Unauthorized' })
     return
   }
